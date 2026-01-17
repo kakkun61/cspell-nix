b7eb766e7366ccb646b89ac8090673c6f1bee987
Kazuki Okamoto (岡本和樹) <kazuki.okamoto@kakkun61.com>
2026-01-18 07:54:02 +0900

    generating a module options documentation

--- flake.nix
+++ flake.nix
@@ -18,7 +18,10 @@
       treefmt-nix,
     }:
     let
-      flakeModule = import ./flake-module.nix;
+      perSystemModule = import ./flake-module.nix { inherit self; };
+      flakeModule = {
+        options.perSystem = flake-parts.lib.mkPerSystemOption perSystemModule;
+      };
     in
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
@@ -31,9 +34,11 @@
           pkgs,
           lib,
           config,
+          system,
           ...
         }:
         {
+          packages.optionsDoc = pkgs.callPackage ./options-doc.nix { inherit perSystemModule; };
           treefmt = {
             programs = {
               nixfmt.enable = true;
